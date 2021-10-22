fee405629ee35bf2ccdebe4bb21126b1a6b06b79
YAOYELU <luyaoyejeren@gmail.com>
2021-10-22 18:31:56 +0800

    [WIP] 添加基础home页面

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrapper">
+    <div class="hd-lt">
+      <img :src="Logo" />
+      <div class="logo-text">
+        <div class="title">WhichUA</div>
+        <div class="desc">Detect every phone</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "@/assets/images/logo.png";
+
+export default {
+  data() {
+    return {
+      Logo,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.wrapper {
+  width: 100%;
+  height: 60px;
+  padding: 0 20px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  .hd-lt {
+    display: flex;
+    align-items: center;
+    img {
+      width: 32px;
+      height: 32px;
+    }
+
+    .logo-text {
+      margin-left: 12px;
+      font-family: Arial, Helvetica, sans-serif;
+      text-align: left;
+      .title {
+        font-weight: 700;
+        font-size: 15px;
+      }
+      .desc {
+        font-size: 13px;
+        font-weight: 500;
+        margin-top: 4px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="page">
+    <Header />
+    <div class="page-top" :style="`background-image:url(${HomeDefaultBg})`">
+      <el-input class="search-bar" placeholder="请输入机型" v-model="content">
+        <i slot="prefix" class="el-input__icon el-icon-search"></i>
+      </el-input>
+    </div>
+    <div class="page-btm"></div>
+  </div>
+</template>
+
+<script>
+import HomeDefaultBg from "@/assets/images/home-default-bg.jpg";
+import Header from "@/components/layouts/Header.vue";
+
+export default {
+  components: { Header },
+  data() {
+    return {
+      HomeDefaultBg,
+      content: "",
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.page {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  &-top {
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 600px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .search-bar {
+      width: 650px;
+    }
+  }
+}
+</style>
