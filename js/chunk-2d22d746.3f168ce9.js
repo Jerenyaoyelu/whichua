b140404da5d7395e51d01ead640031f974f24097
Jerenyaoyelu <45114867+Jerenyaoyelu@users.noreply.github.com>
2021-10-24 13:24:24 +0000

    Deploying to gh-pages from @ Jerenyaoyelu/whichua@7311e9f7b884abc7c9aa7e232f7513771b6c18f2 🚀

---
+++ js/chunk-2d22d746.3f168ce9.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" Hi there, My name is Jeren! ")])},r=[],a={},i=a,s=t("2877"),u=Object(s["a"])(i,c,r,!1,null,"fb8ae2ec",null);n["default"]=u.exports}}]);
