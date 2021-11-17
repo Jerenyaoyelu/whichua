b6023ebb75013a6bf7e7c74baf0d94c92990d18a
YAOYELU <luyaoyejeren@gmail.com>
2021-11-17 16:07:55 +0800

    chore 优化首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
     </div>
     <div class="page-btm">
       <div class="section">
-        <div class="title">What We Cover</div>
-        <div class="content">
+        <div class="desc">
+          <div class="title">What We Cover</div>
           <p class="left">
             本站涵盖了目前国内流行的所有手机品牌和部分型号，截至目前共收录了<span
               >{{ totalBrands }}</span
@@ -14,9 +14,18 @@
             >种手机型号，和<span>{{ totalBrowers }}</span
             >条不同浏览器UA信息，共各位看官查看，且还在不断更新中...
           </p>
-          <div class="right">
-            <BrandsPie :data="brandsList" />
-          </div>
+        </div>
+        <div class="right">
+          <BrandsPie :data="brandsList" />
+        </div>
+      </div>
+      <div class="section">
+        <div class="right"></div>
+        <div class="desc">
+          <div class="title">What We Build</div>
+          <p class="left">
+            基于本站的数据优势，量身打造了更加精准的responsiveJS依赖包，探测设备是否是移动端还是PC端，检验屏幕是否横屏抑或竖屏
+          </p>
         </div>
       </div>
     </div>
@@ -82,26 +91,29 @@ export default {
   &-btm {
     padding: 40px 50px;
     .section {
+      margin-top: 10px;
+      margin-bottom: 100px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .desc {
+        text-align: left;
+      }
       .title {
-        font-size: 60px;
+        margin-bottom: 30px;
+        font-size: 48px;
         font-family: PingFangSC-Medium, PingFang SC;
       }
       .left {
         font-family: PingFangSC-Regular, PingFang SC;
         font-size: 24px;
         width: 600px;
-        text-align: left;
         line-height: 48px;
         span {
           color: #308ce8;
           font-size: 36px;
           text-decoration: underline;
         }
-      }
-      .content {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
       }
     }
   }
